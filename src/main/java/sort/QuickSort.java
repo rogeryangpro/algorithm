e18d5509556a12a150ff003304289ec5ca7860a2
cheng.yang <cheng.yang@qunar.com>
2019-06-03 21:45:19 +0800

    add quick sort

---
+++ src/main/java/sort/QuickSort.java
@@ -0,0 +1,35 @@
+package sort;
+
+import java.util.List;
+
+public class QuickSort implements Sort{
+    @Override
+    public List<Integer> sort(List<Integer> array) {
+        return null;
+
+
+    }
+
+
+
+
+    private void quickSort(List<Integer> array) {
+
+    }
+
+    private int partiton(List<Integer> array, int p, int r) {
+
+
+        int i = p-1;
+
+        for (int j = 1; j < r; j++) {
+            
+        }
+    }
+
+
+
+
+
+
+}
